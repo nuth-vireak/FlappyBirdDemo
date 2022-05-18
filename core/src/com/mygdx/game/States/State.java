249fa50db12e4030c2489f97906a741e1db1d3d9
Nuth Vireak <timberr.4t@gmail.com>
2022-05-18 19:48:28 +0700

    Game State

--- core/src/com/mygdx/game/States/State.java
+++ core/src/com/mygdx/game/States/State.java
@@ -6,18 +6,18 @@ import com.badlogic.gdx.math.Vector3;
 
 public abstract class State {
 
-//    protected OrthographicCamera cam;
-//    protected Vector3 mouse;
-//    protected GameStateManager gsm;
-//
-//    protected State(GameStateManager gsm) {
-//
-//        this.gsm = gsm;
-//        cam = new OrthographicCamera();
-//        mouse = new Vector3();
-//    }
-//
-//    protected abstract void handleInput();
-//    public abstract void update(float dt);
-//    public abstract void render(SpriteBatch sb);
+    protected OrthographicCamera cam;
+    protected Vector3 mouse;
+    protected GameStateManager gsm;
+
+    protected State(GameStateManager gsm) {
+
+        this.gsm = gsm;
+        cam = new OrthographicCamera();
+        mouse = new Vector3();
+    }
+
+    protected abstract void handleInput();
+    public abstract void update(float dt);
+    public abstract void render(SpriteBatch sb);
 }
